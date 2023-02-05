2df994ae4dbfb2bb3b4128d747b1508415645230
Aleksandr Bondarenko <dcomrad@gmail.com>
2023-02-05 07:02:35 +0100

    Исправлено в соответствии с замечаниями ревьюера

--- pep_parse/database.py
+++ pep_parse/database.py
@@ -5,7 +5,6 @@ from pep_parse.constants import DB_URL
 
 
 class Base:
-    @staticmethod
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from pep_parse.constants import BASE_DIR, DATETIME_FORMAT
 from pep_parse.database import Pep, create_db
 
-filename_template = 'status_summary_{}.csv'
+FILENAME_TEMPLATE = 'status_summary_{}.csv'
 
 
 def prepare_to_write(data):
@@ -13,13 +13,16 @@ def prepare_to_write(data):
 
 def write_to_csv(data):
     results_dir = BASE_DIR / 'results'
+    # Не могу перенести results_dir, см. предыдущий комментарий ниже.
+
     # Невозможно перенести эту функцию и/или перенести переменную в constants
     # по той же причине, которая была мной описана в предыдущем задании.
     # Тестирующая система создаёт временную папку в $TMP и не может найти
-    # файлы вывода, если results_dir объявлен вне файла pipelines.py
+    # файлы вывода, если results_dir объявлен вне файла pipelines.py,
+    # т.к. фактически файл создаётся не в $TMP, а в results папки проекта.
     results_dir.mkdir(exist_ok=True)
     now_formatted = datetime.now().strftime(DATETIME_FORMAT)
-    filename = filename_template.format(now_formatted)
+    filename = FILENAME_TEMPLATE.format(now_formatted)
     file_path = results_dir / filename
     with open(file_path, 'w', encoding='UTF-8') as file:
         writer = csv.writer(file, dialect=csv.unix_dialect)
@@ -38,10 +41,11 @@ class PepParsePipeline:
         )
 
         self.session.add(quote)
-        self.session.commit()
         return item
 
     def close_spider(self, spider):
+        self.session.commit()
+
         statuses = [
             item[0] for item in self.session.query(Pep.status).distinct().all()
         ]
